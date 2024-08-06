3d3535166f01b66761cc2e547adde9a905ae45c8
LucasMota08 <lucasmota2004silva@gmail.com>
2024-08-06 17:05:22 -0300

    desenhosportfolio 1.6 image transition

--- script.js
+++ script.js
@@ -21,3 +21,33 @@ function closeImage() {
     var expandedImg = document.getElementById("expanded-img");
     expandedImg.src = '';
 }
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const imageUrls = [
+        '../desenhos/20240311_183229fl.jpg',
+        '../desenhos/20230119_185556bss.jpg',
+        '../desenhos/20231026_145357.jpg',
+        '../desenhos/20231124_102852bss.jpg'
+    ];
+
+    let currentIndex = 0;
+
+    function showNextBackgroundImage() {
+        const backgrounds = document.querySelectorAll('.background-image');
+        backgrounds.forEach((bg, index) => {
+            if (index === (currentIndex % backgrounds.length)) {
+                bg.style.backgroundImage = `url(${imageUrls[currentIndex]})`;
+                bg.style.opacity = 1;
+            } else {
+                bg.style.opacity = 0;
+            }
+        });
+
+        currentIndex = (currentIndex + 1) % imageUrls.length;
+    }
+
+    setInterval(showNextBackgroundImage, 4000);
+});
+
